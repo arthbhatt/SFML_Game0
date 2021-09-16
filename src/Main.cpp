13739c8ead0e5155dc15916a9c31c694ca2c1d3f
arthbhatt <arth3697@gmail.com>
2021-09-15 18:15:40 -0700

    Creates Game class to break down main into smaller encapsulations

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,41 +1,79 @@
 #include "Platform/Platform.hpp"
 
-int main()
+class Game
 {
-	util::Platform platform;
+public:
+	Game();
+	void run();
 
+private:
+	void processEvents();
+	void update();
+	void render();
+
+private:
+	util::Platform platform;
+	sf::Texture mPlayerTexture;
+
+	sf::RenderWindow mWindow;
+	sf::CircleShape mPlayer;
+};
+
+Game::Game()
+{
 #if defined(_DEBUG)
 	std::cout << "Hello World!" << std::endl;
 #endif
 
-	sf::RenderWindow window;
 	// in Windows at least, this must be called before creating the window
-	float screenScalingFactor = platform.getScreenScalingFactor(window.getSystemHandle());
+	float screenScalingFactor = platform.getScreenScalingFactor(mWindow.getSystemHandle());
 	// Use the screenScalingFactor
-	window.create(sf::VideoMode(200.0f * screenScalingFactor, 200.0f * screenScalingFactor), "SFML works!");
-	platform.setIcon(window.getSystemHandle());
+	mWindow.create(sf::VideoMode(200.0f * screenScalingFactor, 200.0f * screenScalingFactor), "SFML works!");
+	platform.setIcon(mWindow.getSystemHandle());
 
-	sf::CircleShape shape(window.getSize().x / 2);
-	shape.setFillColor(sf::Color::White);
+	mPlayer = sf::CircleShape(mWindow.getSize().x / 2);
+	mPlayer.setFillColor(sf::Color::White);
 
-	sf::Texture shapeTexture;
-	shapeTexture.loadFromFile("content/sfml.png");
-	shape.setTexture(&shapeTexture);
+	mPlayerTexture.loadFromFile("content/sfml.png");
+	mPlayer.setTexture(&mPlayerTexture);
+}
 
+void Game::run()
+{
+	while (mWindow.isOpen())
+	{
+		update();
+		render();
+	}
+}
+
+void Game::update()
+{
+	processEvents();
+}
+
+void Game::processEvents()
+{
 	sf::Event event;
 
-	while (window.isOpen())
+	while (mWindow.pollEvent(event))
 	{
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-
-		window.clear();
-		window.draw(shape);
-		window.display();
+		if (event.type == sf::Event::Closed)
+			mWindow.close();
 	}
+}
+
+void Game::render()
+{
+	mWindow.clear();
+	mWindow.draw(mPlayer);
+	mWindow.display();
+}
+
+int main()
+{
+	Game game;
+	game.run();
 
 	return 0;
 }
